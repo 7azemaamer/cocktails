14ce426ca0683be0a97f05fae6bfb7f22419039b
Hazem Aamer <zicoaamer@gmail.com>
2025-06-30 15:16:38 +0300

    Implemented the first part of the hero section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { RouterView } from "vue-router";
+import Navbar from "./components/layout/Navbar.vue";
+import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 </script>
 
 <template>
-  <div>
+  <Navbar />
+  <main>
     <RouterView />
-  </div>
+  </main>
 </template>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,2 +1,46 @@
-<script setup></script>
-<template>This is Fixed cool and naviagted navbar</template>
+<script setup>
+import { onMounted } from "vue";
+import { navLinks } from "../../constants";
+
+import gsap from "gsap";
+
+onMounted(() => {
+  const navTween = gsap.timeline({
+    scrollTrigger: {
+      trigger: "nav",
+      start: "bottom top",
+    },
+  });
+
+  navTween.fromTo(
+    "nav",
+    {
+      backgroundColor: "transparent",
+    },
+    {
+      backgroundColor: "#00000050",
+      backgroundFilter: "blur(10px)",
+      duration: 1,
+      ease: "power1.inOut",
+    }
+  );
+});
+</script>
+
+<template>
+  <nav>
+    <div>
+      <a href="home" class="flex items-center gap-2">
+        <img src="/public/images/logo.png" />
+        <p>Velvet Pour</p>
+      </a>
+      <ul>
+        <li v-for="nav in navLinks" :key="nav.id">
+          <a :href="nav.link" class="text-white">
+            {{ nav.title }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
